ea2846372c972aee41141234970ac2dff874b5fd
tejaswar rao <tejaswar.rao@nybinfotech.com>
2026-07-14 11:04:41 +0530

    deletedfiles

--- React/Client/src/pages/EmployeeDashboard_13-07-2026/EmployeeDashboard.jsx
+++ React/Client/src/pages/EmployeeDashboard_13-07-2026/EmployeeDashboard.jsx
@@ -1,14 +1,14 @@
-import AppProvider, { AppContext } from "../../Components/ContextAPIDemo/AppContext";
+import AppProvider, { AppContext } from "../../Components/EmployeeDashboard/AppContext";
 import React, { useContext } from 'react'
-import Navbar from "../../Components/ContextAPIDemo/Navbar";
-import Sidebar from "../../Components/ContextAPIDemo/Sidebar";
-import Dashboard from "../../Components/ContextAPIDemo/Dashboard";
-import ThemeSwitcher from "../../Components/ContextAPIDemo/ThemeSwitcher";
-import LoginFrom from "../../Components/ContextAPIDemo/LoginForm";
-import UserProfile from "../../Components/ContextAPIDemo/UserProfile";
-import HomeContent from "../../Components/ContextAPIDemo/HomeContent";
-import "../../Components/ContextAPIDemo/ContextDemo.css";
-import ConsumerExample from "../../Components/ContextAPIDemo/ConsumerExample";
+import Navbar from "../../Components/EmployeeDashboard/Navbar";
+import Sidebar from "../../Components/EmployeeDashboard/Sidebar";
+import Dashboard from "../../Components/EmployeeDashboard/Dashboard";
+import ThemeSwitcher from "../../Components/EmployeeDashboard/ThemeSwitcher";
+import LoginFrom from "../../Components/EmployeeDashboard/LoginForm";
+import UserProfile from "../../Components/EmployeeDashboard/UserProfile";
+import HomeContent from "../../Components/EmployeeDashboard/HomeContent";
+import "../../Components/EmployeeDashboard/ContextDemo.css";
+import ConsumerExample from "../../Components/EmployeeDashboard/ConsumerExample";
 
 function ContextContent() {
 
